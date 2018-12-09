91599211ae4b4e3d378019b678d2a13781b330c2
Michael Slattery <michael.slattery@teamwork.com>
2018-12-09 19:06:13 +0000

    Consuming now works

--- src/main/java/com/assignment2/controllers/ProjectController.java
+++ src/main/java/com/assignment2/controllers/ProjectController.java
@@ -1,10 +1,14 @@
 package com.assignment2.controllers;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -21,17 +25,37 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 public class ProjectController {
+	
+	HttpHeaders createHeaders(String username, String password)
+	{
+		return new HttpHeaders() 
+		{
+			{
+				String auth = username + ":" + password;
+				byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("UTF-8")));
+				String authHeader = "Basic " + new String( encodedAuth );
+				System.out.println("\n\n\n" + authHeader + "\n\n\n");
+				set( "Authorization", authHeader );
+			}
+		};
+	}
 
 	@GetMapping("showprojects")
 	public String showAllProjects(Model model) {
 		
 		RestTemplate restTemplate = new RestTemplate();
+		String URL = "http://localhost:8080/api/projects";
+		String username = "michael.slattery1@mycit.ie";
+		String password = "Password";
+		
 		ResponseEntity<List<Project>> responseEntity = restTemplate
-				.exchange("http://localhost:8080/projects", HttpMethod.GET, null, new ParameterizedTypeReference<List<Project>>(){});
+				.exchange(URL, HttpMethod.GET,
+						new HttpEntity<Project>(createHeaders(username, password)),
+						new ParameterizedTypeReference<List<Project>>(){});
 		
 		List<Project> projects = responseEntity.getBody();
 		
-		model.addAttribute("project", projects);
+		model.addAttribute("projects", projects);
 		
 		return "projects";
 	}
